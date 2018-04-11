b7fc4fe5d893ba6e54644fad60fdc65287868c4f
Mohammad Farhad Hossain <hossainmfbd@gmail.com>
2018-04-11 02:14:59 +0200

    Second Commit

--- app/src/androidTest/java/com/mytaxi/android_demo/MainActivityTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/MainActivityTest.java
@@ -6,15 +6,23 @@ import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.mytaxi.android_demo.activities.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,11 +33,13 @@ import org.junit.After;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -38,6 +48,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.Espresso.pressBack;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -65,6 +76,16 @@ public class MainActivityTest {
     @Test
     public void demoTest() {
         login();
+//        ViewInteraction appCompatAutoCompleteTextView = onView(
+//                allOf(withId(R.id.textSearch),
+//                        childAtPosition(
+//                                allOf(withId(R.id.searchContainer),
+//                                        childAtPosition(
+//                                                withClassName(Matchers.is("android.support.design.widget.CoordinatorLayout")),
+//                                                1)),
+//                                0),
+//                        isDisplayed()));
+//        appCompatAutoCompleteTextView.perform(typeText("sa"), closeSoftKeyboard());
 
 //        onView(withId(R.id.textSearch)).perform(click());
 //        onView(withId(R.id.textSearch)).perform(typeText(searchname), closeSoftKeyboard());
@@ -93,6 +114,25 @@ public class MainActivityTest {
     public void afterActivity() {
 //        mActivityRule.getActivity();
 //        Espresso.pressBack();
-        logout();
+        //logout();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
